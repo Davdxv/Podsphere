b89e46ffc9e91ff1fe2e272c3695e72e777105b6
David <davdms@pm.me>
2022-09-02 14:02:44 +0200

    Improve search bar usability

--- src/components/layout/header-component.jsx
+++ src/components/layout/header-component.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import { InputGroup, Form, Container } from 'react-bootstrap';
 import { Box } from '@mui/material';
 import { ToastContext } from '../../providers/toast';
-import RssButton from '../buttons/rss-button';
+import ClearButton from '../buttons/clear-button';
 import SyncButton from '../buttons/sync-button';
 import RefreshButton from '../buttons/refresh-button';
 import SearchButton from '../buttons/search-button';
@@ -12,20 +12,35 @@ import { ReactComponent as AppIcon } from '../../assets/arsync-logo.svg';
 function HeaderComponent({ onSubmit }) {
   const toast = useContext(ToastContext);
   const [isSearching, setIsSearching] = useState(false);
+  const [showClearButton, setShowClearButton] = useState(false);
   const SEARCH_TEXT = 'Search for podcasts, episodes or enter an RSS feed URL to subscribe to';
+  const searchFormRef = useRef();
+
+  const clearSearchForm = () => {
+    if (searchFormRef.current) searchFormRef.current.reset();
+    setShowClearButton(false);
+  };
+
+  const getSearchFormInput = () => {
+    if (searchFormRef.current) {
+      const fd = new FormData(searchFormRef.current);
+      return fd.get('query') || '';
+    }
+    return '';
+  };
+
+  function handleChange() {
+    setShowClearButton(!!getSearchFormInput());
+  }
 
   async function handleSubmit(event) {
     event.preventDefault();
-    const fd = new FormData(event.target);
-    const query = fd.get('query');
+    const query = getSearchFormInput();
     if (query) {
       setIsSearching(true);
       try {
         const handleSearchResult = await onSubmit({ query });
-        if (handleSearchResult) {
-          // Clear search field
-          event.target.reset();
-        }
+        if (handleSearchResult) clearSearchForm(); // On successful subscription to RSS feed
       } catch (ex) {
         console.error(ex);
         toast('Could not find podcast.', { variant: 'danger' });
@@ -42,18 +57,19 @@ function HeaderComponent({ onSubmit }) {
       </Box>
       <Box className={style['form-layer']}>
         <Box>
-          <SearchButton />
+          <SearchButton disabled={isSearching} form="search-form" onClick={handleSubmit} />
         </Box>
         <Box className={style['form-wrapper']}>
-          <Form onSubmit={handleSubmit}>
+          <Form ref={searchFormRef} id="search-form" onSubmit={handleSubmit}>
             <Form.Group controlId="query">
               <InputGroup>
                 <Form.Control
                   name="query"
-                  style={{ paddingLeft: 0 }}
+                  style={{ paddingLeft: '0.2rem' }}
                   placeholder={SEARCH_TEXT}
+                  onChange={handleChange}
                 />
-                <RssButton disabled={isSearching} />
+                {showClearButton && <ClearButton onClick={clearSearchForm} />}
               </InputGroup>
             </Form.Group>
           </Form>
